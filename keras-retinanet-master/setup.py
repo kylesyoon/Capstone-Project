bd8f1ec7596b495cd4c2ee0060eddadd4fb60c61
Kyle Yoon <apps.yoon@gmail.com>
2018-01-04 17:35:15 -0600

    Added keras-retinanet

---
+++ keras-retinanet-master/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='keras-retinanet',
+    version='0.0.1',
+    url='https://github.com/fizyr/keras-retinanet',
+    author='Hans Gaiser',
+    author_email='h.gaiser@fizyr.com',
+    maintainer='Hans Gaiser',
+    maintainer_email='h.gaiser@fizyr.com',
+    packages=setuptools.find_packages(),
+    install_requires=['keras', 'keras-resnet', 'six'],
+    entry_points = {
+        'console_scripts': [
+            'retinanet-train=keras_retinanet.bin.train:main',
+            'retinanet-evaluate-coco=keras_retinanet.bin.evaluate_coco:main',
+        ],
+    }
+)
